66b6cfdf20dfe18292011ad84c8096f626162143
github-action-benchmark <github@users.noreply.github.com>
2026-06-26 01:07:53 +0000

    add dev-mcxa (customSmallerIsBetter) benchmark result for 8705e326ecb29e4074feaa73542245b2df716c99

--- dev/binsize/data.js
+++ dev/binsize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782436043112,
+  "lastUpdate": 1782436073286,
   "repoUrl": "https://github.com/dymk/odp-embedded-controller",
   "entries": {
     "dev-npcx": [
@@ -807,6 +807,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/dymk/odp-embedded-controller/commit/d6fb3ce5d9ae52ca51d6ef7b87518c6c4cb3c809"
         },
         "date": 1780004820946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size",
+            "value": 78.81,
+            "unit": "KiB",
+            "extra": "RAM Size: 14.99 KiB\nDependency Count: 328\nVersion: rustc 1.96.0 (ac68faa20 2026-05-25)"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kdinelle@microsoft.com",
+            "name": "Kurtis Dinelle",
+            "username": "kurtjd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "8705e326ecb29e4074feaa73542245b2df716c99",
+          "message": "dev-qemu: Update HAL and use custom QEMU (#28)\n\n* dev-qemu: Update HAL and use custom QEMU\n\n* Replace 'virt' with 'ec'",
+          "timestamp": "2026-06-18T14:50:24-07:00",
+          "tree_id": "76cad2ba1ca1e9b1c062a1eef1466a28d6cf86d2",
+          "url": "https://github.com/dymk/odp-embedded-controller/commit/8705e326ecb29e4074feaa73542245b2df716c99"
+        },
+        "date": 1782436072853,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
